d4df058fb3fd6486823f1f0df84fe23caed14228
kim-kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-12-07 22:30:18 +0100

    Create InputField.tsx

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import {useState} from "react";
+
+interface InputFieldProps {
+    id: string,
+    type: string,
+    placeholder: string,
+    onChange?: (value: string) => void
+}
+
+export default function InputField({id, type, placeholder, onChange}: InputFieldProps) {
+    const [value, setValue] = useState("");
+
+    const handleChange = (e: any) => {
+        const newValue = e.target.value
+        setValue(newValue)
+        if(onChange) onChange(newValue)
+    }
+
+    return (
+        <input
+            className="text-darkBrown border border-darkBrown rounded-lg bg-transparent p-2 w-full"
+            id={id}
+            type={type}
+            placeholder={placeholder}
+            onChange={handleChange}
+            value={value}
+        />
+    );
+}
