06091303c08bece748248df04858770f5be81b72
kim-kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-12-08 08:40:52 +0100

    Add button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React from "react";
+
+interface ButtonProps {
+    label?: string;
+    onClick: () => void;
+    type?: "button" | "submit" | "reset";
+    className?: string;
+}
+
+export default ({
+    label,
+    onClick,
+    type = "button",
+    className = "",
+                }: ButtonProps) => (
+    <button
+        type={type}
+        onClick={onClick}
+        className={`bg-[#8A5E3D] font-serif text-lg py-2 px-4 rounded-full ${className} border border-[#D4AF37]`}
+    >
+        {label}
+    </button>
+)
--- src/components/NewsletterSignUp/NewsletterSignUp.tsx
+++ src/components/NewsletterSignUp/NewsletterSignUp.tsx
@@ -1,9 +1,23 @@
 "use client";
 
-export default function NewsletterSignUp() {return (
+import InputField from "@/components/InputField/InputField";
+import Button from "@/components/Button/Button";
+
+export default function NewsletterSignUp() {
+    const handleInputChange = (value: string) => {
+        console.log(value);
+    }
+
+    return (
         <div className="bg-beige p-4 rounded-xl shadow-md">
             <div className="rounded-lg border-double border-4 border-darkBrown p-4">
-                <h1 className="text-center text-darkBrown text-4xl font-serif">Gyllene Droppen</h1>
+                <div className="grid gap-4">
+                    <h1 className="text-center text-darkBrown text-4xl font-serif">Gyllene Droppen</h1>
+                    <InputField id={"userEmail"} type={"email"} placeholder={"Din e-postadress"} onChange={handleInputChange}  />
+                    <div className="grid justify-center">
+                        <Button label="Skicka" onClick={() => console.log('ok')} />
+                    </div>
+                </div>
             </div>
         </div>
     );
